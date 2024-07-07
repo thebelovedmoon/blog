1ed00d1910805b8be2c60aeb859a80d084bf605a
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2024-07-07 18:33:26 +0800

    some stuff update

---
+++ blog-assets/js.js
@@ -0,0 +1,10 @@
+function init() { // start page
+  detectsNewTab();
+}
+
+//code from https://stackoverflow.com/questions/850058/is-it-possible-to-detect-if-a-user-has-opened-a-link-in-a-new-tab
+function detectsNewTab() {
+  if (!(history.length == 1)) {
+    document.getElementById("goBack").style.display = "flex";
+  }
+}
